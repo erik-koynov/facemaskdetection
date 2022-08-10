4ae2ef597ae182250d9087df657a9551e6cfc5d2
koynov <koynov@lexoro.ai>
2022-08-10 13:35:26 +0300

    custom dataset class and utils for loading and plotting

---
+++ dataset.py
@@ -0,0 +1,79 @@
+from torch.utils.data import Dataset
+import pandas as pd
+from utils import resize_img_and_boxes, GaussianNoise
+import numpy as np
+from typing import List
+import imgaug.augmenters as iaa
+import torch
+import cv2
+from torchvision.transforms import RandomGrayscale, GaussianBlur, ColorJitter, Compose, ToTensor
+
+class FaceMaskVOCDataset(Dataset):
+    def __init__(self, dataset: pd.DataFrame, out_width: int, out_height:int, training = True):
+        self.dataset = dataset.reset_index(drop=True)
+        self.out_width = out_width
+        self.out_height = out_height
+        self.transform = iaa.Sequential([
+            iaa.Affine(translate_percent={"x": (-0.3, 0.3), "y": (-0.1, 0.1)},scale=(0.7, 1),),
+            iaa.PerspectiveTransform(scale=(0, 0.1)),
+
+        ])
+        #self.resize = iaa.Resize({"height":self.out_height, "width": self.out_width}, interpolation='linear'),
+
+        self.color_augment = Compose([
+            ToTensor(),
+            RandomGrayscale(p=0.05),
+            GaussianBlur(kernel_size=3,sigma=(0.001, 0.5)),
+            ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
+            GaussianNoise(std=0.5, p=0.05)
+        ])
+
+        self.training = training
+
+
+    def __getitem__(self, idx: int):
+        image_path, bboxes, labels= self.dataset.iloc[idx].values
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        image, bboxes = resize_img_and_boxes(image, bboxes, (self.out_height, self.out_width))
+
+        if self.training:
+            image, bboxes = self._apply_transforms(image, bboxes)
+            image_tensor = self.color_augment(image)
+
+        else:
+            image_tensor = ToTensor()(image)
+
+        targets = self._format_targets_as_dict(bboxes=bboxes, labels=labels, idx=idx)
+
+        return image_tensor, targets
+
+
+    def _apply_transforms(self, image: np.ndarray, bboxes: List[List[int]]):
+        img_aug, bbox = self.transform (image=image,
+            bounding_boxes=np.array(bboxes)[None, ...])
+        return img_aug, bbox[0]
+
+    def _format_targets_as_dict(self, bboxes: List[List[int]], labels: List[int], idx: int):
+        boxes = torch.FloatTensor(bboxes)
+
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        #print(labels)
+        labels = torch.LongTensor(labels)
+
+        image_id = torch.tensor([idx])
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        target["image_id"] = image_id
+
+        return target
+
+    def __len__(self):
+        return len(self.dataset)
---
+++ utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+from typing import List, Tuple
+import cv2
+import torch
+from torch.optim.lr_scheduler import LambdaLR
+from torchvision.ops import nms
+
+class GaussianNoise:
+    def __init__(self, mean=0., std=1., p=0.):
+        self.std = std
+        self.mean = mean
+        self.p = p
+
+    def __call__(self, tensor):
+        if np.random.rand(1) > 1-self.p:
+            return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        return tensor
+
+
+def resize_img_and_boxes(img: np.ndarray, bbox: List[List[int]], new_size: Tuple[int]):
+    """
+    Albumentations function does not transform the bboxes...
+    :param img: the image (h w c)
+    :param bbox: [[bbox1],[bbox2]]
+    :param new_size: h, w
+    :return:
+    """
+    h, w = img.shape[:-1]
+    new_h, new_w = new_size
+
+    new_bboxes = []
+    for box in bbox:
+        xmin, ymin, xmax, ymax = box
+
+        xmin = (xmin / w) * new_w
+        ymin = (ymin / h) * new_h
+
+        xmax = (xmax / w) * new_w
+        ymax = (ymax / h) * new_h
+
+        new_bboxes.append([xmin, ymin, xmax, ymax])
+
+    return cv2.resize(img, new_size), new_bboxes
+
+def draw_basic_bbox(image: np.array, bboxes):
+    image = image.copy()
+    for bbox in bboxes:
+        #print(bbox)
+        cv2.rectangle(image, np.array(bbox[:2]).round().astype(int), np.array(bbox[2:]).round().astype(int),
+                      color=(0, 255, 0), thickness=2)
+    return image
+
+def linear_learning_rate_warmup(optimizer, num_warmup_steps, num_training_steps, min_factor = 0.0):
+    # a learning rate that decreases linearly from the initial lr set in the optimizer to lr*min_factor, after
+    # a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
+    # num_training_steps = int(n_batches / grad_acc_steps=1) * n_epochs
+
+    optimizer = optimizer
+
+    return LambdaLR(optimizer, linear_warmup_factor_wrapper(num_warmup_steps, num_training_steps, min_factor), -1)
+
+def linear_warmup_factor_wrapper(num_warmup_steps, num_training_steps, min_factor=0.0):
+    num_warmup_steps = num_warmup_steps
+    num_training_steps = num_training_steps
+    def lr_lambda(current_step: int):
+        #  computes a multiplicative factor given an integer parameter for the current (epoch)
+        if current_step <= num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        return max(
+            min_factor, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))
+        )
+    return lr_lambda
+
+def non_max_suppression(preds: dict, iou_threshold: float = 0.25, obj_score_thresh = 0.0)->Tuple[np.ndarray]:
+    bboxes = preds['boxes']
+    labels = preds['labels']
+    scores = preds['scores']
+    objectness_mask = scores>obj_score_thresh
+    keep_indices = nms(bboxes[objectness_mask], scores[objectness_mask], iou_threshold)
+
+    return bboxes[keep_indices].detach().numpy(), labels[keep_indices].detach().numpy(), scores[keep_indices].detach().numpy()
